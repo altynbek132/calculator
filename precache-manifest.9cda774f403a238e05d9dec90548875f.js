e15ffa0f813d2736226b082db84659ddd8e80fba
Altynbek <altynbek.ktl@gmail.com>
2020-06-02 14:14:49 +0600

    Updates

---
+++ precache-manifest.9cda774f403a238e05d9dec90548875f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c503564822b06349ef643e80a45c5db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cec813fa1a26faba64ed",
+    "url": "/static/css/main.795f9c6e.chunk.css"
+  },
+  {
+    "revision": "69c5341de25dab04569f",
+    "url": "/static/js/2.57d533f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57d533f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec813fa1a26faba64ed",
+    "url": "/static/js/main.8d3d9c39.chunk.js"
+  },
+  {
+    "revision": "e3607995a7bd8aea3c2e",
+    "url": "/static/js/runtime-main.4f2002da.js"
+  }
+]);
